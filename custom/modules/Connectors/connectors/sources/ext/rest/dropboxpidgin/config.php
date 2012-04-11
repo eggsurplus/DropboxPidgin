5d706411437c01c2919b14419f004eeb5dff6c44
eggsurplus <junk@eggsurplus.com>
2012-04-11 10:20:09 -0500

    Removed bad config

--- custom/modules/Connectors/connectors/sources/ext/rest/dropboxpidgin/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/***CONNECTOR SOURCE***/
-$config['name'] = 'DropboxPidgin';
-$config['eapm']['enabled'] = true;
-$config['order'] = 10;
-$config['properties']['dropboxpidigin_api_url'] = 'https://api.dropbox.com/1/';
-$config['properties']['dropboxpidigin_api_content_url'] = 'https://api-content.dropbox.com/1/';
-$config['properties']['oauth_consumer_key'] = '2rblv3hlgta2haj';
-$config['properties']['oauth_consumer_secret'] = '37qf08odl5olwfc';
