b50f7671dadd736197911bca2586fde80c813214
eggsurplus <junk@eggsurplus.com>
2012-04-11 10:20:54 -0500

    Config

---
+++ custom/modules/Connectors/connectors/sources/ext/rest/dropboxpidgin/config.php
@@ -0,0 +1,9 @@
+<?php
+/***CONNECTOR SOURCE***/
+$config['name'] = 'DropboxPidgin';
+$config['eapm']['enabled'] = true;
+$config['order'] = 10;
+$config['properties']['dropboxpidigin_api_url'] = 'https://api.dropbox.com/1/';
+$config['properties']['dropboxpidigin_api_content_url'] = 'https://api-content.dropbox.com/1/';
+$config['properties']['oauth_consumer_key'] = 'PUT_YOUR_KEY_HERE';
+$config['properties']['oauth_consumer_secret'] = 'PUT_YOUR_SECRET_HERE';
